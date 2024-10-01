d079259f9f242219fc37b194c911c7404fcb9b67
Jatin030303 <jatinrude751@gmail.com>
2024-10-01 10:05:25 +0530

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,32 @@
+<?php
+?>
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="../css/footer.css">
+
+</head>
+<body>
+<footer class="footer">
+          <div class="footer_container">
+            <div class="social_icon">
+              <a href="https://www.facebook.com/people/AutoCare-Pro/61559231237794/"><i class="fa-brands fa-facebook"></i></a>
+              <a href="https://www.instagram.com/autocarepro03?igsh=MWVmbThscG85ZDg4NA=="><i class="fa-brands fa-instagram"></i></a>
+              <a href=""><i class="fa-brands fa-twitter"></i></a>
+              <a href=""><i class="fa-brands fa-google-plus"></i></a>
+              <a href=""><i class="fa-brands fa-youtube"></i></a>
+            </div>
+            <div class="footer_nav">
+              <ul>
+                <li><a href="">Home</a></li>
+                
+                <li><a href="../html/about.php">About</a></li>
+                <li><a href="../html/contact.php">Contact Us</a></li>
+                <li><a href="../html/ourteam.php">Our Team </a></li>
+                <li><a href="../admin/loginform.php">Admin Login</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="footer_bottom">
+            <p>Copyright  &copy;2024; Designed by <span class="designer">AJ</span></p>
+          </div>
+          </footer>
---
+++ include/header.php
@@ -0,0 +1,48 @@
+<?php
+?>
+   <link rel="stylesheet" href="../css/header.css">
+
+<header class="header">
+    <div class="container">
+
+        <a href="#" class="logo">
+            <img src="../images/logo.png" width="128" height="63" alt="autofix home">
+        </a>
+
+        <nav class="navbar" data-navbar>
+            <ul class="navbar-list">
+                <li>
+                    <a href="../index.php" class="navbar-link">Home<span></span></a>
+                </li>
+
+                <li>
+                    <a href="../html/about.php" class="navbar-link">About<span></span></a>
+                </li>
+
+                <li>
+                    <a href="#bookmark" class="navbar-link">Services<span></span></a>
+                </li>
+
+                <li>
+                    <a href="../html/gallery.php" class="navbar-link">Gallery<span></span></a>
+                </li>
+
+                <li>
+                    <a href="../html/contact.php" class="navbar-link">Contact<span></span></a>
+                </li>
+                
+            </ul>
+        </nav>
+
+        <a href="../html/userlogin.php" class="btn1">
+            <span class="span">Login</span>
+            <div class="liquid"></div>
+        </a>
+
+        <button class="nav-toggle-btn" aria-label="toggle menu" data-nav-toggler>
+            <span class="nav-toggle-icon icon-1"></span>
+            <span class="nav-toggle-icon icon-2"></span>
+            <span class="nav-toggle-icon icon-3"></span>
+        </button>
+    </div>
+</header>
