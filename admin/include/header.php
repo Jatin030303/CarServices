41999f48ec8da9965c1b24c5b3a45f29b5afeff3
Jatin030303 <jatinrude751@gmail.com>
2024-10-01 10:38:44 +0530

    Add files via upload

---
+++ admin/include/header.php
@@ -0,0 +1,32 @@
+<?php
+?>
+<link rel="Shortcut icon" href="../img/admm.png" type="image/x-icon">
+  <link rel="stylesheet" href="../css/header.css">
+<div class="side-menu">
+        <div class="brand-name">
+            <li><img src="../img/logo.png" style="height: 50px;" alt=""></li>
+            <h1>AutoCare-Pro</h1>
+        </div>
+        <ul>
+            <li><img src="../img/dashboard.png" style="height: 50px;" alt="">&nbsp; <span><a href="../admin_dashboard.php">Dashboard</a></span></li>
+            <li><img src="../img/service2.png" style="height: 50px;" alt="">&nbsp;<span><a href="../service_req/service_dashboard.php">Service Request</a></span></li>
+            <li><img src="../img/mechanic1.png" style="height: 50px;" alt="">&nbsp;<span><a href="../mechanic_list/mechanic_dashboard.php">Mechanic List</a></span></li>
+            <li><img src="../img/driver.png" style="height: 50px;" alt="">&nbsp;<span><a href="../driver/driver_dashboard.php">Driver</a></span></li>
+            <li><img src="../img/feedback.png" style="height: 50px;" alt="">&nbsp;<span><a href="../feedback/feedback_dashboard.php">Feedback</a></span></li>
+        </ul>
+    </div>
+    <div class="container">
+        <div class="header">
+            <div class="nav">
+                <div class="search">
+                    <input type="text" placeholder="Search..">
+                    <button type="submit"><img src="../img/search.png" alt=""></button>
+                </div>
+                <div class="user">
+                    <a href="../profile.php" class="btn" id="admin-btn">Administrator Admin</a>
+                    <div class="img-case">
+                        <img src="../img/user.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
